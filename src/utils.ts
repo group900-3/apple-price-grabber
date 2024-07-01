ca26f7dbaba869e23f05576a3498aa1604c449f0
witcat <witcat@nine00.com>
2024-07-02 00:11:00 +0800

    retry on failed

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,10 @@
-export const loadContents = (url: string) => fetch(url).then((r) => r.text());
+import pRetry, { AbortError } from "p-retry";
+
+export const loadContents = (url: string) => {
+  const func = async () => {
+    const res = await fetch(url);
+    if (res.status === 404) throw new AbortError(res.statusText);
+    return res.text();
+  };
+  return pRetry(func, { retries: 2 });
+};
