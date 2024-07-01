31327cb1977200af7f1099264dd45711c2e6c773
witcat <witcat@nine00.com>
2024-07-02 00:01:26 +0800

    fill selectors

---
+++ src/selectors.ts
@@ -0,0 +1,27 @@
+export const macbook_pro =
+  "div[data-core-tabs-panels] > div > div > div:nth-child(1) > div > div > div.rc-productbundle-pricecontainer.typography-label > div > div.rc-price > div > span";
+export const macbook_air = macbook_pro;
+
+export const imac =
+  "#root > div.rs-bundleselection > div.rc-productselection-grid.as-l-container > div.row > div:nth-child(1) > div > div > div.rc-productbundle-pricecontainer.typography-label > div > div.rc-price > div > span";
+export const mac_pro = imac;
+export const mac_studio = imac;
+export const mac_mini = imac;
+
+export const airpods =
+  "#root > div.rf-pdp.as-l-container.row > div.column.large-4.small-12 > div.rf-pdp-prices > div > div.rc-price > div > span";
+
+export const airpods_max =
+  "#root > div > div.rf-flagship-buyflow.as-l-container.row > div.column.large-5.large-offset-1.small-offset-0.small-12.rf-flagship-buyflow-columnright > div.rf-flagship-summary-wrapper > div.rf-flagship-summary > div > div.rc-summary-price-box > div > div.rc-price > div > span > span.price-point.price-point-fullPrice-short";
+export const homepod = airpods_max;
+export const homepod_mini = airpods_max;
+
+export const ipad_pro =
+  "#root > div.rf-bfe-stickybar > div > div:nth-child(1) > div.rf-bfe-stickybar-header > div > div.rf-bfe-header-price-wrapper > div > div > div.rc-price > div > span > span.price-point.price-point-fullPrice > span";
+export const ipad_air = ipad_pro;
+export const ipad = ipad_pro;
+export const ipad_mini = ipad_pro;
+
+export const iphone_15 = ipad_pro;
+export const iphone_15_pro = ipad_pro;
+export const iphone_se = ipad_pro;
